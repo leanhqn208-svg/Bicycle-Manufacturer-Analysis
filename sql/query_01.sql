c9244671fe9cf50b8983c5ca7a4225bb6f585ecd
leanhqn208-svg <leanhqn208@gmail.com>
2026-04-03 21:16:53 +0700

    Create query_01.sql

---
+++ sql/query_01.sql
@@ -0,0 +1,18 @@
+--QUERY 1
+-- Calc Quantity of items, Sales value & Order quantity by each Subcategory in L12M-----
+
+select format_datetime('%b %Y', a.ModifiedDate) month
+      ,c.Name
+      ,sum(a.OrderQty) qty_item
+      ,sum(a.LineTotal) total_sales
+      ,count(distinct a.SalesOrderID) order_cnt
+FROM `adventureworks2019.Sales.SalesOrderDetail` a 
+left join `adventureworks2019.Production.Product` b
+  on a.ProductID = b.ProductID
+left join `adventureworks2019.Production.ProductSubcategory` c
+  on b.ProductSubcategoryID = cast(c.ProductSubcategoryID as string)
+
+where date(a.ModifiedDate) >=  (select date_sub(date(max(a.ModifiedDate)), INTERVAL 12 month)
+                                from `adventureworks2019.Sales.SalesOrderDetail` )
+group by 1,2
+order by 2,1;
